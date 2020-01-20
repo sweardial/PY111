2a43084a52f300af71130884bb7a57cd63c298e8
sweardial <dimon4ikches@yandex.ru>
2020-01-20 18:32:02 +0300

    сделал

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -5,11 +5,10 @@ from typing import Sequence
 
 def min_search(arr: Sequence) -> int:
 	min = arr[0]
-	id = 0
+	ind_position = 0
 	for ind, el in enumerate(arr):
 		if el<min:
 			min = el
-			id = ind
-	return id
-
+			ind_position = ind
+	return ind_position
 
